2d04a6465f57d9b6525f59ca41218dedfd6b60f1
Chetneetchouhan <Chetneetchouhan01@gmail.com>
2022-01-09 19:12:49 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Enter your first number")
+
+input1 = input()
+
+print("and now enter your second number")
+input2 = input()
+
+print("The addition of number you given (which are, ",int(input1), " and ", int(input2), ") is this", int(input1) + int(input2))
+print("The subtraction of those two numbers are", int(input1) - int(input2))
+print("The multiplication of those two numbers are", int(input1) * int(input2))
+print("The division of those two numbers are", int(input1) / int(input2))
